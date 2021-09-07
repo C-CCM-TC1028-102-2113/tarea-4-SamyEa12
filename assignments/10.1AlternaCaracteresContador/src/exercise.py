797465b088f16bb1f75d57e620609a9f3a4904dc
SamyEa12 <77683636+SamyEa12@users.noreply.github.com>
2021-09-07 12:41:25 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    n=int(input())
+    for i in range(1, n+1):
+        if i%2==0:
+            x="%"
+        elif i%2!=0:
+            x="#"
+        print(str(i)+"-"+str(x))
     pass
 
 if __name__=='__main__':   
