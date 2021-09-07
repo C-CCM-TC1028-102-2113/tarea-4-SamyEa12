b3484c00721455f03995eefbb14e842fffe9adc2
SamyEa12 <77683636+SamyEa12@users.noreply.github.com>
2021-09-07 15:10:08 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -1,7 +1,20 @@
 
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    num = int(input("Enter a number: "))
+    x=0
+    y=1
+    z=int()
+    a=1
+    if num==0:
+        print(x)
+    while a<num:
+        z=y+x
+        x=y
+        y=z
+        a=a+1
+        if a>=num:
+            print(z)
 
 if __name__=='__main__':
     main()
